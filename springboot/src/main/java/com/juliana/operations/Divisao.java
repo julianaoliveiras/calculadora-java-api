417b5a54e046da41380ae28324263c77410c674b
Juliana Oliveira <h4bituei.o@gmail.com>
2022-06-19 22:57:18 -0300

    criação das operações básicas

---
+++ springboot/src/main/java/com/juliana/operations/Divisao.java
@@ -0,0 +1,10 @@
+package com.juliana.operations;
+
+public class Divisao implements Operacao{
+    
+    @Override
+    public double calculo(double valor1, double valor2) {
+        return valor1 / valor2;
+    }
+    
+}
---
+++ springboot/src/main/java/com/juliana/operations/Operacao.java
@@ -0,0 +1,5 @@
+package com.juliana.operations;
+
+public interface Operacao {
+    double calculo(double valor1, double valor2);
+}
